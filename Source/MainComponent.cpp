8be23c72ea5cba5f0c807e7b08eb8ea736ea3a96
fyefaq <fyestaken@gmail.com>
2023-09-25 16:42:48 -0300

    Changed Font Size

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,12 +16,12 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
+    g.setFont (juce::Font (32.0f));
     g.setColour (juce::Colours::white);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
-void MainComponent::resized()
+void MainComponent::resized()  
 {
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
